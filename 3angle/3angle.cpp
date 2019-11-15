5335ae59521bcf1cf38567415f789d7987ea641d
Валерия Томилова <madame.valeriya.tomilova@yandex.ru>
2019-11-15 14:55:20 +0500

    v4

--- 3angle/3angle.cpp
+++ 3angle/3angle.cpp
@@ -189,3 +189,129 @@ void comparision(double& a, double& b, double& c, double& xr, double& yr, double
 
 
 
+double deg_x(double a, double b, double c, double xr, double yr, double zr)
+{
+	if ((yr != 0) && (zr != 0)) xr = M_PI - yr - zr; else
+		if ((a != 0) && (b != 0) && (c != 0)) xr = acos((a * a + c * c - b * b) / (2 * a * c)); else
+			if ((b != 0) && (a != 0) && (zr != 0)) xr = asin(b * sin(zr) / a); else
+				if ((b != 0) && (a != 0) && (yr != 0)) xr = asin(b * sin(yr) / a);
+
+	return xr;
+}
+
+double deg_y(double a, double b, double c, double xr, double yr, double zr)
+{
+	if ((xr != 0) && (zr != 0)) yr = M_PI - xr - zr; else
+		if ((a != 0) && (b != 0) && (c != 0)) yr = acos((a * a + b * b - c * c) / (2 * a * b)); else
+			if ((b != 0) && (c != 0) && (xr != 0)) yr = asin(c * sin(xr) / b); else
+				if ((a != 0) && (c != 0) && (zr != 0)) yr = c * sin(zr) / a; if (yr > 1) yr = 1; yr = asin(yr);
+
+	return yr;
+
+}
+
+double deg_z(double a, double b, double c, double xr, double yr, double zr)
+{
+	if ((a != 0) && (b != 0) && (xr != 0)) zr = asin(a * sin(xr) / b); else
+		if ((xr != 0) && (yr != 0)) zr = M_PI - xr - yr; else
+			if ((a != 0) && (c != 0) && (yr != 0)) zr = asin(a * sin(yr) / c);
+
+	return zr;
+
+}
+
+double side_a(double a, double b, double c, double xr, double yr, double zr)
+{
+	if ((b != 0) && (c != 0)) a = sqrt(b * b + c * c - 2 * b * c * cos(zr)); else
+		if ((b != 0) && (yr != 0) && (zr != 0)) a = b * sin(zr) / sin(xr); else
+			if ((c != 0) && (xr != 0) && (zr != 0)) a = c * sin(zr) / sin(yr);
+
+	return a;
+}
+
+double side_b(double a, double b, double c, double xr, double yr, double zr)
+{
+	if ((a != 0) && (c != 0) && (xr != 0)) b = sqrt(a * a + c * c - 2 * a * c * cos(xr)); else
+		if ((a != 0) && (xr != 0) && (yr != 0)) b = a * sin(xr) / sin(zr);
+
+	return b;
+}
+
+double side_c(double a, double b, double c, double xr, double yr, double zr)
+{
+	if ((a != 0) && (b != 0) && (yr != 0)) c = sqrt(a * a + b * b - 2 * a * b * cos(yr));
+
+	return c;
+}
+
+void if_degrees(double& ar, double xr, double yr, double zr, double& xg, double& xm, double& xs, double& yg, double& ym, double& ys, double& zg, double& zs, double& zm)
+{
+	double  ag, am, as;
+	for (int i = 0; i < 3; i++) {
+		if (i == 0) {
+			ar = xr;
+			xg = conversion_deg(ar);
+			xm = conversion_min(ar);
+			xs = conversion_sec(ar);
+		}
+		else
+			if (i == 1) {
+				ar = yr;
+				yg = conversion_deg(ar);
+				ym = conversion_min(ar);
+				ys = conversion_sec(ar);
+			}
+			else
+				if (i == 2) {
+					ar = zr;
+					zg = conversion_deg(ar);
+					zm = conversion_min(ar);
+					zs = conversion_sec(ar);
+				}
+	}
+}
+
+double conversion_deg(double ar)
+{
+	double ag;
+
+	ar *= DMS;
+	ag = floor(ar);
+
+	return ag;
+}
+
+double conversion_min(double ar)
+{
+	double am, ag;
+
+	ar *= DMS;
+	ag = floor(ar);
+	am = (ar - ag) * 60;
+	am = floor(am);
+
+	return am;
+}
+
+double conversion_sec(double ar)
+{
+	double as, am, ag, a1;
+
+	ar *= DMS;
+	ag = floor(ar);
+	a1 = (ar - ag) * 60;
+	am = floor(a1);
+	as = (a1 - am) * 60;
+
+	return as;
+}
+
+void conclusion(double a, double b, double c, double xg, double xm, double xs, double yg, double ym, double ys, double zg, double zs, double zm) //Вывод данных
+{
+	cout << "a=" << a << endl;
+	cout << "b=" << b << endl;
+	cout << "c=" << c << endl;
+	cout << "x=" << xg << char(248) << xm << "\'" << xs << "\"" << endl;
+	cout << "y=" << yg << char(248) << ym << "\'" << ys << "\"" << endl;
+	cout << "z=" << zg << char(248) << zs << "\'" << zm << "\"" << endl;
+}
